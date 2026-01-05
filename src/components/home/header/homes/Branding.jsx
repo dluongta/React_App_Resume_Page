a745dc2a3502e2462556320564b819083eeab565
Luong <ctpluong@gmail.com>
2026-01-05 17:12:35 +0700

    complete app

--- src/components/home/header/homes/Branding.jsx
+++ src/components/home/header/homes/Branding.jsx
@@ -62,8 +62,8 @@ export const Branding = ({ className }) => {
 
       <div style={{
         position: 'relative',
-        width: '80%',
-        height: '110vh', 
+        width: '75%',
+        height: '75vh',
         overflow: 'hidden',
         background: '#000',
         margin: '0 auto' ,
