8ef8c475109967b0bcdc08d45c5e3fd392d80e66
Luong <ctpluong@gmail.com>
2026-04-15 14:05:38 +0700

    complete app

--- src/components/home/header/homes/Home.jsx
+++ src/components/home/header/homes/Home.jsx
@@ -247,7 +247,7 @@ export const Home = ({ className }) => {
             <div className="description-text">
               <p>
                 I am Dinh Luong Ta. I am a programmer skilled at Software Development like Web, Android, Computer System And Application Development. I am also learning about Artificial Intelligence and Hardware.
-                I am extremely fascinated by science and technology. All of my products are from the ULTRAMIND generation, made by DLUONGTA.
+                I am extremely fascinated by science, engineering and technology. All of my products are from the ULTRAMIND generation, made by DLUONGTA.
               </p>
 
               <p>
