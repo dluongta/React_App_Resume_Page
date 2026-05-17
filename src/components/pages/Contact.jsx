b376ea484f73406a75ea4516b58c35631f3aec1f
Luong <ctpluong@gmail.com>
2026-05-17 18:51:36 +0700

    complete app

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 export const Contact = () => {
     return (
-        <>
-            <section className="contact">
-                {/* <div id="mySidenav" className="sidenav">
-                    <Link to='/pages' id="pages"> Pages </Link>
-                    <Link to='/blog' id="blog"> Blog </Link>
-                    <Link to='/portfolio' id="portfolio"> Portfolio </Link>
-                    <Link to='/contact' id="contact"> Contact </Link>
-                    <Link to='/galaxy' id="galaxy"> Galaxy </Link>
-                </div> */}
-                <div className="pdf-container">
-                    <iframe className="pdf" src="/dluongta_resume.pdf" width="100%" height="100%"></iframe>
-                </div>
-            </section>
-        </>
+        <section className="contact contact-page">
+            <div className="pdf-container">
+                <iframe
+                    className="pdf"
+                    src="/dluongta_resume.pdf"
+                    title="resume"
+                />
+            </div>
+
+            <div style={{ height: '120px' }} />
+        </section>
     );
-}
+};
