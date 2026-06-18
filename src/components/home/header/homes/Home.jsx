cc7902f378a0743900b17dc3c81b32118e741721
Luong <94155158+luen2003@users.noreply.github.com>
2026-06-18 09:03:46 +0000

    complete app

--- src/components/home/header/homes/Home.jsx
+++ src/components/home/header/homes/Home.jsx
@@ -444,7 +444,7 @@ export const Home = ({ className }) => {
                   lineHeight: '1.2',
                   // Mã màu gradient: cam, đỏ, hồng, tím, xanh
                   // background: 'linear-gradient(94deg,#fe954a 6.06%,#f84063 34.53%,#ad29e3 66.74%,#008eff 100.96%)',
-                  background: 'linear-gradient( 135deg,  #F58529 0%,  #DD2A7B 50%,  #8134AF 100%)', 
+                  background: 'linear-gradient(135deg, #FF6B00 0%, #FF3366 45%,#7C3AED 100%)', 
                   WebkitBackgroundClip: 'text',
                   WebkitTextFillColor: 'transparent',
                   backgroundClip: 'text',
