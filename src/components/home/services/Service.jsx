813b20f328d4df425e79cc82d3a3a75ebd66cd16
Luong <ctpluong@gmail.com>
2026-07-18 20:06:21 +0700

    complete app

--- src/components/home/services/Service.jsx
+++ src/components/home/services/Service.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import bannerImage from "../../../assets/banner-bg.png";
+import bgImage from "../../../assets/bg.png";
 
 export const Service = ({ className = "" }) => {
   const servicesData = [
@@ -7,37 +7,37 @@ export const Service = ({ className = "" }) => {
       id: 1,
       title: "Frontend Development",
       desc: "Building responsive and interactive user interfaces with advanced web technologies.",
-      cover: bannerImage,
+      cover: bgImage,
     },
     {
       id: 2,
       title: "Backend Development",
       desc: "Developing flexible and scalable server-side infrastructure.",
-      cover: bannerImage,
+      cover: bgImage,
     },
     {
       id: 3,
       title: "Full-Stack Development",
       desc: "Building complete web applications from client-side to server and database.",
-      cover: bannerImage,
+      cover: bgImage,
     },
     {
       id: 4,
       title: "Website Responsive",
       desc: "Creating responsive websites with an intuitive user interface for every device.",
-      cover: bannerImage,
+      cover: bgImage,
     },
     {
       id: 5,
       title: "Website Optimization",
       desc: "Enhancing speed, efficiency, and overall user experience.",
-      cover: bannerImage,
+      cover: bgImage,
     },
     {
       id: 6,
       title: "Website Development",
       desc: "Building and deploying websites with a focus on quality and performance.",
-      cover: bannerImage,
+      cover: bgImage,
     },
   ];
 
